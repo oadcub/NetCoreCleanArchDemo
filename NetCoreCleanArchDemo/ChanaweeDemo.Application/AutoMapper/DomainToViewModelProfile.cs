793e1f68965c97e72fbdda9e7290d07f15da201d
Chanawee <oadcub@gmail.com>
2019-12-02 03:27:35 +0700

    add automapper profile and configuration

---
+++ NetCoreCleanArchDemo/ChanaweeDemo.Application/AutoMapper/DomainToViewModelProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using ChanaweeDemo.Application.ViewModels;
+using ChanaweeDemo.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChanaweeDemo.Application.AutoMapper
+{
+    public class DomainToViewModelProfile : Profile
+    {
+        public DomainToViewModelProfile()
+        {
+            CreateMap<Product, ProductViewModel>();
+        }
+    }
+}
---
+++ NetCoreCleanArchDemo/ChanaweeDemo.Application/AutoMapper/ViewModelToDomainProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using ChanaweeDemo.Application.ViewModels;
+using ChanaweeDemo.Domain.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChanaweeDemo.Application.AutoMapper
+{
+    public class ViewModelToDomainProfile : Profile
+    {
+        public ViewModelToDomainProfile()
+        {
+            CreateMap<ProductViewModel, CreateProductCommand>()
+                .ConstructUsing(c => new CreateProductCommand(c.Name, c.Price));
+        }
+    }
+}
