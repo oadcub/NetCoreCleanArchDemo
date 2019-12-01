273b80955e8ff7baace4313df18bd184e4986162
Chanawee <oadcub@gmail.com>
2019-12-02 01:07:13 +0700

    add product list page

---
+++ NetCoreCleanArchDemo/ChanaweeDemo.Mvc/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChanaweeDemo.Application.Interfaces;
+using ChanaweeDemo.Application.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChanaweeDemo.Mvc.Controllers
+{
+    [Authorize]
+    public class ProductController : Controller
+    {
+        private IProductService _productService;
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        public IActionResult Index()
+        {
+            ProductViewModel model = _productService.GetProducts();
+            return View(model);
+        }
+    }
+}
