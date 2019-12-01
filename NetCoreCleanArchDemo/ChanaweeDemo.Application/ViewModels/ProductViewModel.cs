dbdd7bfebc5509da886b9d2361d887a06b32fdf0
Chanawee <oadcub@gmail.com>
2019-12-02 00:04:10 +0700

    Add IProductRepository, IProductService, ProductViewModel

---
+++ NetCoreCleanArchDemo/ChanaweeDemo.Application/ViewModels/ProductViewModel.cs
@@ -0,0 +1,12 @@
+﻿using ChanaweeDemo.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChanaweeDemo.Application.ViewModels
+{
+    public class ProductViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
---
+++ NetCoreCleanArchDemo/ChanaweeDemo.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using ChanaweeDemo.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChanaweeDemo.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetProducts();
+    }
+}
