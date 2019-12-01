92cecb0ddc2c88d73f0a15c2ed075433a687a034
Chanawee <oadcub@gmail.com>
2019-12-01 23:23:36 +0700

    add EF core

---
+++ NetCoreCleanArchDemo/ChanaweeDemo.Infra.Data/Context/RestaurantDBContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChanaweeDemo.Infra.Data.Context
+{
+    public class RestaurantDBContext : DbContext
+    {
+    }
+}
